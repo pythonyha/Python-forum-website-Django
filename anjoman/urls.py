8fa5062d3ba29a66ccc2302927458ab792ba661b
unknown <mahdizarei.mail@gmail.com>
2018-09-30 22:42:08 +0330

    Addd

--- anjoman/urls.py
+++ anjoman/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from main_app import views
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
+    path('login/',views.login),
+    path('register/', views.register),
 ]

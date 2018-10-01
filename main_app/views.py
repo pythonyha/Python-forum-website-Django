4bbd3433dea5bb0cfd64b818b31bdfffc91cdb33
Mahdi-git <mahdizarei.mail@gmail.com>
2018-10-01 18:09:37 +0330

    Update Login&Registery Pages

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
-     return render(request,"index.html")
+     return render(request,"index.html",context)
 
 def register(request):
      return render(request,"register.html")
 
 def login(request):
      return render(request,"login.html")
-
+context = {"titlepre":"فعلا یه صفحه ورود و یه صفحه ثبت نام داریم"}
